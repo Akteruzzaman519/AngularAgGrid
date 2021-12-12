869e51d19ac6198258c0a6cf91650addc6829703
Akteruzzaman519 <mdakteruzzaman519@gmail.com>
2021-12-12 22:31:25 +0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AdnanComponent } from './adnan/adnan.component';
+import { RituComponent } from './ritu/ritu.component';
+import { NaimComponent } from './naim/naim.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AkterComponent } from './akter/akter.component';
+import { SonetComponent } from './sonet/sonet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdnanComponent,
+    RituComponent,
+    NaimComponent,
+    AkterComponent,
+    SonetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
