74ab6024322409a39cc0ab63dd85cc4a5d80b4b6
ics <icsadmin20@gamil.com>
2021-12-23 16:59:01 +0600

    accordion  and tab ar add here

--- src/app/uifeature/uifeature.component.ts
+++ src/app/uifeature/uifeature.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UIFeatureComponent implements OnInit {
 
+  acc = document.getElementsByClassName("accordion");
+
   constructor() { }
 
   ngOnInit(): void {
+    document.getElementById("defaultOpen").click();
+  }
+  public accordion(acc: any, i) {
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+  }
+
+  public Acc() {
+    this.accordion(this.acc, 0);
+  }
+
+
+  public openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
   }
 
 }
